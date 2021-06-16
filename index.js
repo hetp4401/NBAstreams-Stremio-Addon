86b1038e7c7811b809023bafe57e008cb69928a7
hetp4401 <patelhet32@gmail.com>
2021-06-15 23:58:22 -0400

    test

--- index.js
+++ index.js
@@ -62,11 +62,13 @@ addon.get("/stream/:type/:id.json", (req, res) => {
     .wrap(game, () => getStreams(game), { ttl: 60 * 15 })
     .then((streams) => {
       respond(res, {
-        streams: streams.map((x) => ({
-          name: "NBAstreams",
-          title: title,
-          url: x,
-        })),
+        streams: [{ name: "hi", title: "erfer", url: "rgergergregeg" }],
+
+        // streams.map((x) => ({
+        //   name: "NBAstreams",
+        //   title: title,
+        //   url: x,
+        // })),
       });
     });
 });
@@ -75,5 +77,6 @@ function respond(res, data) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Content-Type", "application/json");
+  res.setHeader("Cache-Control", "public, max-age=3600");
   res.send(data);
 }
